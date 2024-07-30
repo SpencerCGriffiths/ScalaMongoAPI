860d019b60ffc80ca19867f25a6629ba0aa40c4e
Spencer Griffiths <s.clarkegriffiths@gmail.com>
2024-07-30 10:30:19 +0100

    PlaceHolder tests for Application Controller

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,5 +1,29 @@
 package controllers
 
-class ApplicationControllerSpec {
+import baseSpec.BaseSpecWithApplication
+
+class ApplicationControllerSpec extends BaseSpecWithApplication{
+
+  val TestApplicationController = new ApplicationController(
+    component
+  )
+
+  "ApplicationController .index()" should {
+
+  }
+
+  "ApplicationController .create()" should {
+
+  }
+
+  "ApplicationController .read()" should {
+
+  }
+  "ApplicationController .update()" should {
+
+  }
+  "ApplicationController .delete()" should {
+
+  }
 
 }
